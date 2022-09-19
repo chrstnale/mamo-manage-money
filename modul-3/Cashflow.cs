907cf265b03c5e27ea1bcfa0775ff7fb9f6b71ee
Hapsari P <hapsari.ndhari7@gmail.com>
2022-09-19 13:05:57 +0700

    Menambahkan class Cashflow.cs

---
+++ modul-3/Cashflow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class Cashflow
+{
+	private string _type;
+	private float _nominal;
+	private string _category;
+	private string _notes;
+	private string _date;
+
+	public string type
+    {
+		get { return _type; }
+    }
+	public float nominal
+    {
+		get { return _nominal; }
+		set { _nominal = value; }
+    }
+	public string category
+    {
+		get { return _category; }
+		set { _category = value; }
+    }
+	public string notes
+    {
+		get { return _notes; }
+		set { _notes = value; }
+    }
+	public string date
+    {
+		get { return _date; }
+		set { _date = value; }
+    }
+}
