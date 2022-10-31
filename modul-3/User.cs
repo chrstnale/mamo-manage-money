e7984952b3a21f60b1ed42634c52aecde397f44d
Hapsarip <hapsari.ndhari7@gmail.com>
2022-10-31 16:32:11 +0700

    ulang upload file

---
+++ modul-3/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace modul_3
+{
+    class User
+    {
+        private int _id;
+        private string _name;
+        private string _email;
+        private string _password;
+        private float _balance;
+        public int id { get { return _id; } }
+        public string name 
+        { 
+            get { return _name; }
+            set { _name = value; } 
+        }
+        public string email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public float balance { get { return _balance; } }
+        public void changeName(string name)
+        {
+            name = _name;
+        }
+    }
+}
