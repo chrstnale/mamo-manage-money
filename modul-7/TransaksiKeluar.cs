cd11b478f0f8ffe17f75c9bf91b3d3842ff84e44
mfarrasi <farras.ilhami@mail.ugm.ac.id>
2022-11-08 12:02:31 +0700

    Inheritance, Encapsulation, dan Polymorphism

---
+++ modul-7/TransaksiKeluar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul_7
+{
+    internal class TransaksiKeluar : TransaksiClass
+    {
+        public TransaksiKeluar(int uang) : base(uang)
+        {
+            Uang = uang;
+        }
+        public override void Transaksi(int jumlah)
+        {
+            Uang -= jumlah;
+        }
+    }
+}
---
+++ modul-7/TransaksiMasuk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul_7
+{
+    internal class TransaksiMasuk : TransaksiClass
+    {
+        public TransaksiMasuk(int uang) : base(uang)
+        {
+            Uang = uang;
+        }
+        public override void Transaksi(int jumlah)
+        {
+            Uang += jumlah;
+        }
+    }
+}
